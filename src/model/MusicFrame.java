508a1d883fd2e68b83d9be9991d9061d1f928932
imtinti <iuri.tinti7@gmail.com>
2019-06-12 19:53:13 -0300

    diagrama de classes atualizado

--- src/model/MusicFrame.java
+++ src/model/MusicFrame.java
@@ -1,5 +1,28 @@
 package model;
 
 public class MusicFrame {
+	
+	private Oitava Oitava;
+	private Nota Nota;
+	private Instrumento Instrumento;
+	private BPM BPM;
+	
+	
+	public MusicFrame() {
+		
+		
+		
+	}
+	
+	public MusicFrame(model.Oitava oitava, model.Nota nota, model.Instrumento instrumento, model.BPM bPM) {
+
+		this.Oitava = oitava;
+		this.Nota = nota;
+		this.Instrumento = instrumento;
+		this.BPM= bPM;
+		
+	}
+	
+	
 
 }
