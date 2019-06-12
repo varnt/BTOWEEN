3b3f3b0b7fe98adb3e50dfac37f3160847ccca42
Victor Arnt <35453021+varnt@users.noreply.github.com>
2019-06-12 02:32:53 -0300

    Asta + musica
    Adcionado diagrama asta e classe musica, aguardando diagrama de classes atualizado

---
+++ src/model/Musica.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Musica {
+
+	public Musica() {
+		// TODO Auto-generated constructor stub
+		int ListaMusicFrames
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
