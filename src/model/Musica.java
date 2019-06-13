f138326e0eea0574c951d7735979c6519bc61e42
imtinti <iuri.tinti7@gmail.com>
2019-06-12 22:21:01 -0300

    continuar classe musica

--- src/model/Musica.java
+++ src/model/Musica.java
@@ -6,6 +6,11 @@ public class Musica {
 	private MusicFrame atualFrame;
 	ArrayList<MusicFrame> listaFrames = new ArrayList <MusicFrame>();
 	
+	//para cada mudança em um dos parametros da musica deve
+	//ser criado um novo elemento Frame para ser adicionado na lista
+	
+	
+	
 	
 	
 }
