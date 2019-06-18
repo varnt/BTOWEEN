2cd0d04bdfd1341c924c8551400c664e16ed26a5
Victor Arnt <35453021+varnt@users.noreply.github.com>
2019-06-17 22:43:27 -0300

    Update MusicaLivre.java

--- src/controlller/MusicaLivre.java
+++ src/controlller/MusicaLivre.java
@@ -1,20 +1,33 @@
 package controlller;
 
-import java.util.ArrayList;
-
-import model.MusicFrame;
+import java.io.*;
 
 public class MusicaLivre {
+	
+	private char[] atualmusica;
 
 	public MusicaLivre() {
 		// TODO Auto-generated constructor stub
 	
-		private ArrayList<char> atualmusica;
+		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("teste");
 
+	}
+	public void LeEntrada(String CaminhoArquivo) throws Exception{    
+	          FileReader fr=new FileReader(CaminhoArquivo);    
+	          BufferedReader br=new BufferedReader(fr);    
+	  
+	          int i;    
+	          while((i=br.read())!=-1){  
+	          System.out.print((char)i);  
+	          }  
+	          br.close();    
+	          fr.close();    
+	        
 	}
 
 }
