4c1da3810cd30be7628ca2c1bb7ddd32d29455fe
Victor Arnt <35453021+varnt@users.noreply.github.com>
2019-06-17 21:37:05 -0300

    GUI base adicionada

--- src/controlller/MusicaLivre.java
+++ src/controlller/MusicaLivre.java
@@ -1,9 +1,15 @@
 package controlller;
 
+import java.util.ArrayList;
+
+import model.MusicFrame;
+
 public class MusicaLivre {
 
 	public MusicaLivre() {
 		// TODO Auto-generated constructor stub
+	
+		private ArrayList<char> atualmusica;
 	}
 
 	public static void main(String[] args) {
