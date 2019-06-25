cd2f2bf201e9f59945a251f55af4fa9f5e5331b0
Victor Arnt <35453021+varnt@users.noreply.github.com>
2019-06-24 22:48:30 -0300

    UIImplementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,40 +1,42 @@
 package application;
+
 import java.util.Scanner;
 import model.MusicFrame;
 import model.Musica;
 import controlller.Arquivo;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Main {
 
 	public static void main(String[] args) {
-				
+
 		//Arquivo arquivo = new Arquivo();
 		Musica musica = new Musica();
-		//Arquivo arquivo = new Arquivo();
-		
-		
-	Scanner ler = new Scanner(System.in);		
-	System.out.println("Digite o comando: ");
-	char caractereAnterior = 'a';
-	for(int i =0 ; i<10; i++) {
-	char entrada = ler.nextLine().charAt(0);
-	musica.manipulaMusica(entrada, caractereAnterior);
-	
-	caractereAnterior = entrada;
-	
-	if(musica.ehNota(entrada))
-		musica.executaFrameAtual();	 //ignorar caso caractere nao seja nota
-	
-	System.out.println(musica.getAtualFrame());
-	
+		// Arquivo arquivo = new Arquivo();
+
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite o comando: ");
+		char caractereAnterior = 'a';
+		for (int i = 0; i < 10; i++) {
+			char entrada = ler.nextLine().charAt(0);
+			musica.manipulaMusica(entrada, caractereAnterior);
+
+			caractereAnterior = entrada;
+
+			if (musica.ehNota(entrada))
+				musica.executaFrameAtual(); // ignorar caso caractere nao seja nota
+
+			System.out.println(musica.getAtualFrame());
+
+		}
+
+		System.out.println("Elementos na lista: ");
+		for (MusicFrame model : musica.getListaFrames()) {
+			System.out.println(model.toString());
+		}
+
+		ler.close();
 	}
-	
-	System.out.println("Elementos na lista: ");
-	for(MusicFrame model : musica.getListaFrames()) {
-		System.out.println(model.toString());
-	}
-	
-	ler.close();
-}
 
 }
