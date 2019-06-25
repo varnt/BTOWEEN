d0236e6f1e1ad6dcd6b8a4d058c9b0539dd36eb2
Victor Arnt <35453021+varnt@users.noreply.github.com>
2019-06-25 00:50:01 -0300

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,45 +1,58 @@
 package application;
 
-import java.util.Scanner;
-import model.MusicFrame;
-import model.Musica;
-import controlller.Arquivo;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.io.IOException;
 
-public class Main {
+import controlller.Arquivo;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import model.Musica;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage)
+	{
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource("/view/View.fxml"));
+			Scene scene = new Scene(parent);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
+		launch(args);
 
-		
 		Arquivo arquivo = new Arquivo();
 		Musica musica = new Musica();
 		// Arquivo arquivo = new Arquivo();
 
-/*		Scanner ler = new Scanner(System.in);
-		System.out.println("Digite o comando: ");
-		char caractereAnterior = 'a';
-		for (int i = 0; i < 10; i++) {
-			char entrada = ler.nextLine().charAt(0);
-			musica.manipulaMusica(entrada, caractereAnterior);
+		/*
+		 * Scanner ler = new Scanner(System.in);
+		 * System.out.println("Digite o comando: "); char caractereAnterior = 'a'; for
+		 * (int i = 0; i < 10; i++) { char entrada = ler.nextLine().charAt(0);
+		 * musica.manipulaMusica(entrada, caractereAnterior);
+		 * 
+		 * caractereAnterior = entrada;
+		 * 
+		 * if (musica.ehNota(entrada)) musica.executaFrameAtual(); // ignorar caso
+		 * caractere nao seja nota
+		 * 
+		 * System.out.println(musica.getAtualFrame());
+		 * 
+		 * }
+		 * 
+		 * System.out.println("Elementos na lista: "); for (MusicFrame model :
+		 * musica.getListaFrames()) { System.out.println(model.toString()); }
+		 * 
+		 * ler.close();
+		 */
 
-			caractereAnterior = entrada;
-
-			if (musica.ehNota(entrada))
-				musica.executaFrameAtual(); // ignorar caso caractere nao seja nota
-
-			System.out.println(musica.getAtualFrame());
-
-		}
-
-		System.out.println("Elementos na lista: ");
-		for (MusicFrame model : musica.getListaFrames()) {
-			System.out.println(model.toString());
-		}
-		
-		ler.close();
-*/
-		
 	}
 
 }
