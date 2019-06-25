61dbe31861eef96f3ad1e1f331af930c402ff415
Victor Arnt <35453021+varnt@users.noreply.github.com>
2019-06-25 03:10:02 -0300

    Update Main.java
    Main com UI embutida

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,36 @@
 package application;
 
+import java.io.IOException;
 import java.util.Scanner;
 import model.MusicFrame;
 import model.Musica;
 import controlller.Arquivo;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+	@Override
+	public void start(Stage stage)
+	{
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource("/view/View.fxml"));
+			Scene scene = new Scene(parent);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
 
-		
-		//Arquivo arquivo = new Arquivo();
-		Musica musica = new Musica();
+		launch(args);
 		// Arquivo arquivo = new Arquivo();
+		Musica musica = new Musica();
+		//Arquivo arquivo = new Arquivo();
 
 		Scanner ler = new Scanner(System.in);
 		System.out.println("Digite o comando: ");
@@ -37,10 +53,9 @@ public class Main {
 			System.out.println(model.toString());
 		}
 		musica.salvaMIDI();
-		
+
 		ler.close();
 
-		
 	}
 
 }
