0eff770fc5ae0b37db973565b9c04c7778626e8b
Victor Arnt <35453021+varnt@users.noreply.github.com>
2019-06-25 03:16:02 -0300

    ATUALIZAÇÃO DE ESCOPO

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Estão encapsulados e não podem ser alterados.
 codigos para notas na java sound:
 60 62 64 65 67 69 71
 do re mi fa sol la si
+
+
+VIEW CONTROLLER É A NOVA MAIN DEVE TER INSTANCIADO:
+
+MUSICA
+ARQUIVO 
+E OUTRAS CLASSES QUE POSSAM CONSUMIR ISSO
--- src/application/Main.java
+++ src/application/Main.java
@@ -27,7 +27,7 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 
-		launch(args);
+		//launch(args); descomentar
 		// Arquivo arquivo = new Arquivo();
 		Musica musica = new Musica();
 		//Arquivo arquivo = new Arquivo();
